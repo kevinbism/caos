50a3c9ccfb0baf2f5b7bea668fe5c7a4e18ada66
Kevin Ramirez <kevinbism@gmail.com>
2023-04-05 15:30:23 +0200

    Add new caos files

--- src/js/caos.js
+++ src/js/caos.js
@@ -1,29 +1,38 @@
-import defaults from './defaults.esm.js'
+let defaults = {
+  init: true,
+  offset: 120,
+  delay: 0,
+  easing: 'ease',
+  duration: 400,
+  disable: false,
+  once: false,
+  startEvent: 'DOMContentLoaded',
+  animatedClassName: 'caos-animate',
+};
 
 class Caos {
-
   constructor(...args) {
-    const caos = this
-    let params = args[0]
+    const caos = this;
+    let params = args[0];
 
-    params = Object.assign(defaults, params !== undefined ? params : {})
+    params = Object.assign(defaults, params !== undefined ? params : {});
 
-    caos.options = params
+    caos.options = params;
 
     if (params.init) {
-      caos.init()
+      caos.init();
     }
   }
 
   init() {
-    const caos = this
+    const caos = this;
     let observer = new IntersectionObserver(caos.visible);
     const targets = [...document.querySelectorAll('[data-caos]')];
 
     targets.map(element => {
       observer.observe(element);
     });
-  };
+  }
 
   visible(entries) {
     entries.map(el => {
@@ -33,7 +42,7 @@ class Caos {
         el.target.classList.remove('caos-animate');
       }
     });
-  };
+  }
 }
 
 export default Caos;
